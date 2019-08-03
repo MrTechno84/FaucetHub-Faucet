3c385d431f0d8c6324d648be65c050b03a5eb3c7
MrTechno84 <41655490+MrTechno84@users.noreply.github.com>
2019-08-03 02:22:45 +0200

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `faucet_banned_address` (
   `id` int(32) unsigned NOT NULL AUTO_INCREMENT,
-  `address` varchar(100) NOT NULL,
+  `faucethub_username` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
@@ -30,13 +30,13 @@ INSERT INTO `faucet_settings` (`id`, `name`, `value`) VALUES
 (2, 'space_top', 'Space on the top'),
 (3, 'space_left', 'Space on the left side'),
 (4, 'space_right', 'Space on the right side'),
-(5, 'timer', '60'),
+(5, 'timer', '15'),
 (6, 'min_reward', '1'),
-(7, 'max_reward', '100'),
+(7, 'max_reward', '20'),
 (8, 'reCaptcha_privKey', ''),
 (9, 'reCaptcha_pubKey', ''),
 (10, 'faucethub_key', ''),
-(11, 'claim_enabled', 'yes'),
+(11, 'claim_enabled', 'no'),
 (12, 'admin_username', 'admin'),
 (13, 'admin_password', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918'),
 (14, 'vpn_shield', 'no'),
@@ -44,9 +44,20 @@ INSERT INTO `faucet_settings` (`id`, `name`, `value`) VALUES
 (16, 'reverse_proxy', 'no'),
 (17, 'admin_login', ''),
 (18, 'auto_withdraw', 'no'),
-(19, 'usr_exp_gain', 'no'),
-(20, 'usr_booster', 'no');
+(19, 'faucet_coupons', 'no');
 
+CREATE TABLE IF NOT EXISTS `faucet_coupons` (
+  `id` int(32) unsigned NOT NULL AUTO_INCREMENT,
+  `coupon_code` VARCHAR(20) NOT NULL,
+  `coupon_value` decimal(10,8) NOT NULL,
+  `coupon_expiration` datetime NOT NULL,
+  `coupon_count` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+/* TESTING */
+INSERT INTO `faucet_coupons` (`id`, `coupon_value`, `coupon_expiration`, `coupon_count`) VALUES
+(1, 'coupon_code_1', '100', '15-July-2019', '10'),
+(2, 'coupon_code_2', '50', '25-July-2019', '20');
 
 CREATE TABLE IF NOT EXISTS `faucet_transactions` (
   `id` int(32) unsigned NOT NULL AUTO_INCREMENT,
@@ -56,7 +67,7 @@ CREATE TABLE IF NOT EXISTS `faucet_transactions` (
   `timestamp` int(32) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
+/* */
 CREATE TABLE IF NOT EXISTS `faucet_user_list` (
   `id` int(32) unsigned NOT NULL AUTO_INCREMENT,
   `faucethub_username` varchar(50) NOT NULL,
@@ -70,8 +81,5 @@ CREATE TABLE IF NOT EXISTS `faucet_user_list` (
   `referred_by` int(32) NOT NULL,
   `last_claim` int(32) NOT NULL,
   `claim_cryptokey` varchar(75) NOT NULL,
-  `is_online` varchar(75) NOT NULL,
-  `is_offline` varchar(75) NOT NULL,
-  `u_exp` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
